f60777a76af98b04ddf71347cd353536354417e6
x0y0z0tn <x0y0z0tn@gmail.com>
2023-06-23 20:27:45 -0400

    example with spherical SDF

--- raymarching.js
+++ raymarching.js
@@ -1,6 +1,10 @@
 const canvas = document.getElementById("glcanvas");
 const gl = canvas.getContext("webgl");
 
+if (!gl) {
+  console.log("Unable to initialize WebGL.");
+}
+
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(
   vertexShader,
@@ -21,33 +25,85 @@ gl.shaderSource(
 
     uniform vec2 resolution;
 
-    float sphereSDF(vec3 p, vec3 c, float r) {
-        return length(p - c) - r;
+    float sphereSDF(vec3 p, float r) {
+        return length(p) - r;
+    }
+
+    float radialSymmetrySDF(vec3 p) {
+      float r = length(p);
+      float ph = atan(p.y, p.x);
+      float th = acos(p.z / r);
+
+      float pattern = r/12.0 * sin(12.0 * ph) * cos(12.0 * th);
+
+      return length(vec2(r-20.0, pattern)) - 0.25;
+    }
+
+    float mapTheWorld(in vec3 p) {
+      return radialSymmetrySDF(p);
+    }
+
+    vec3 calculateNormal(in vec3 p) {
+      const vec3 small_step = vec3(0.001, 0.0, 0.0);
+
+      float gradient_x = mapTheWorld(p + small_step.xyy) - mapTheWorld(p - small_step.xyy);
+      float gradient_y = mapTheWorld(p + small_step.yxy) - mapTheWorld(p - small_step.yxy);
+      float gradient_z = mapTheWorld(p + small_step.yyx) - mapTheWorld(p - small_step.yyx);
+
+      return normalize(vec3(gradient_x, gradient_y, gradient_z));
+    }
+
+    vec3 rayMarch(in vec3 ro, in vec3 rd) {
+      float total_distance_traveled = 0.0;
+      const int number_of_steps = 256;
+      const float minimum_distance = 0.001;
+      const float maximum_distance = 100.0;
+
+      for (int i = 0; i < number_of_steps; i++) {
+        vec3 current_position = ro + total_distance_traveled * rd;
+
+        float distance_to_closest = mapTheWorld(current_position);
+
+        if (distance_to_closest < minimum_distance) {
+          vec3 normal = calculateNormal(current_position);
+          vec3 light_position = vec3(0.0, 0.0, 2.0);
+          vec3 direction_to_light = normalize(current_position - light_position);
+
+          float diffuse_intensity = max(0.0, dot(normal, direction_to_light));
+
+          return vec3(1.0) * diffuse_intensity;
+        }
+
+        if (total_distance_traveled > maximum_distance) {
+          break;
+        }
+
+        total_distance_traveled += distance_to_closest;
+      }
+
+      return vec3(0.25);
     }
 
     void main() {
-        vec2 uv = (gl_FragCoord.xy - 0.5 * resolution) / min(resolution.y, resolution.x);
+      vec2 uv = (gl_FragCoord.xy - 0.5 * resolution) / min(resolution.y, resolution.x);
 
-        vec3 cameraPos = vec3(0.0, 0.0, -5.0);
-        vec3 rayDir = normalize(vec3(uv, 1.0));
-        vec3 rayPos = cameraPos;
-        const int maxSteps = 64;
-        float stepSize = 0.1;
-        for (int i = 0; i < maxSteps; i++) {
-            float dist = sphereSDF(rayPos, vec3(0.0), 1.0);
-            if (dist < 0.01) {
-                float c = 1.0 - min(length(rayPos - cameraPos) / 8.0, 1.0);
-                gl_FragColor = vec4(c, c, c, 1.0);
-                return;
-            }
-            rayPos += dist * rayDir;
-        }
+      vec3 camera_position = vec3(2.0, 0.0, -60.0);
+      vec3 ro = camera_position;
+      vec3 rd = normalize(vec3(uv, 1.0));
 
-        gl_FragColor = vec4(0.0);
+      vec3 color = rayMarch(ro, rd);
+
+      gl_FragColor = vec4(color, 1.0);
     }
 `
 );
 gl.compileShader(fragmentShader);
+if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+  console.error(
+    "error compiling fragment shader",
+    gl.getShaderInfoLog(fragmentShader)
+  );
+}
 
 const program = gl.createProgram();
 gl.attachShader(program, vertexShader);
@@ -75,7 +131,7 @@ function render() {
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-  requestAnimationFrame(render);
+  //requestAnimationFrame(render);
 }
 
 function resize() {
